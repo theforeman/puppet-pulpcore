2fb995f959d56e3ca9634b9914eac3325f1f11c7
William Clark <wclark@redhat.com>
2019-12-13 17:01:25 -0500

    Specify /pulp/api/v3
    Fixes #49

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -1,7 +1,7 @@
 # Configure an Apache vhost
 # @api private
 class pulpcore::apache {
-  $api_path = '/pulp/api'
+  $api_path = '/pulp/api/v3'
   $api_url = "http://${pulpcore::api_host}:${pulpcore::api_port}${api_path}"
   $content_path = '/pulp/content'
   $content_url = "http://${pulpcore::content_host}:${pulpcore::content_port}${content_path}"
