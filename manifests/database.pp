2fa345fcb6b7d1f2f0edc7a183e6a2236f623860
William Bradford Clark <wclark@redhat.com>
2020-02-06 22:53:23 -0500

    Fixes #28901 - support external postgresql

--- manifests/database.pp
+++ manifests/database.pp
@@ -1,17 +1,19 @@
 # Set up the PostgreSQL and Redis databases
 # @api private
 class pulpcore::database {
-  include postgresql::client
-  include postgresql::server
-  postgresql::server::db { $pulpcore::postgresql_db_name:
-    user     => $pulpcore::user,
-    password => postgresql_password($pulpcore::user, $pulpcore::postgresql_db_password),
+  if $pulpcore::postgresql_manage_db {
+    include postgresql::client
+    include postgresql::server
+    postgresql::server::db { $pulpcore::postgresql_db_name:
+      user     => $pulpcore::user,
+      password => postgresql_password($pulpcore::user, $pulpcore::postgresql_db_password),
+      before   => Pulpcore::Admin['migrate --noinput'],
+    }
   }
 
   pulpcore::admin { 'migrate --noinput':
     unless      => 'python3-django-admin migrate --plan | grep "No planned migration operations"',
     refreshonly => false,
-    require     => Postgresql::Server::Db[$pulpcore::postgresql_db_name],
   }
 
   include redis
--- spec/classes/pulpcore_spec.rb
+++ spec/classes/pulpcore_spec.rb
@@ -8,6 +8,8 @@ describe 'pulpcore' do
       it { is_expected.to compile.with_all_deps }
       it { is_expected.to contain_package('python3-pulpcore') }
       it { is_expected.to contain_concat__fragment('base').without_content(/REMOTE_USER_ENVIRON_NAME/) }
+      it { is_expected.to contain_class('postgresql::server') }
+      it { is_expected.to contain_postgresql__server__db('pulpcore') }
 
       context 'with a plugin' do
         let(:pre_condition) { "pulpcore::plugin { 'myplugin': }" }
@@ -17,6 +19,23 @@ describe 'pulpcore' do
           is_expected.to contain_pulpcore__plugin('myplugin')
             .that_subscribes_to('Class[Pulpcore::Install]')
             .that_notifies(['Class[Pulpcore::Database]', 'Class[Pulpcore::Service]'])
+        end
+      end
+
+      context 'with external postgresql' do
+        let :params do
+          {
+            postgresql_db_host: '192.0.2.222',
+            postgresql_manage_db: false,
+          }
+        end
+
+        it do
+          is_expected.to compile.with_all_deps
+          is_expected.not_to contain_class('postgresql::server')
+          is_expected.not_to contain_postgresql__server__db('pulpcore')
+          is_expected.to contain_concat__fragment('base')
+            .with_content(%r{'HOST': '192.0.2.222'})
         end
       end
     end
