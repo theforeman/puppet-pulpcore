ec1480522d237a510b7e1635f31742bac1dc3c29
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-04-12 00:54:28 +0200

    Modulesync: use voxpupuli helper gems

---
+++ spec/setup_acceptance_node.pp
@@ -0,0 +1,37 @@
+$major = $facts['os']['release']['major']
+
+case $major {
+  '7': {
+    package { 'centos-release-scl-rh':
+      ensure => installed,
+    }
+
+    package { 'rh-redis5-redis':
+      ensure  => installed,
+      require => Package['centos-release-scl-rh'],
+    }
+  }
+  '8': {
+    package { 'glibc-langpack-en':
+      ensure => installed,
+    }
+  }
+  default: {}
+}
+
+# Defaults to staging, for release, use
+# $baseurl = "https://fedorapeople.org/groups/katello/releases/yum/nightly/pulpcore/el${major}/x86_64/"
+$baseurl = "http://koji.katello.org/releases/yum/katello-nightly/pulpcore/el${major}/x86_64/"
+
+yumrepo { 'pulpcore':
+  baseurl  => $baseurl,
+  gpgcheck => 0,
+}
+
+# Needed as a workaround for idempotency
+if $facts['os']['selinux']['enabled'] {
+  package { 'pulpcore-selinux':
+    ensure  => installed,
+    require => Yumrepo['candlepin'],
+  }
+}
--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,53 +1,20 @@
-ENV['PUPPET_INSTALL_TYPE'] ||= 'agent'
-ENV['BEAKER_IS_PE'] ||= 'no'
-ENV['BEAKER_PUPPET_COLLECTION'] ||= 'puppet6'
-ENV['BEAKER_debug'] ||= 'true'
+require 'voxpupuli/acceptance/spec_helper_acceptance'
+
 ENV['BEAKER_setfile'] ||= 'centos7-64{hostname=centos7-64.example.com}'
-ENV['BEAKER_HYPERVISOR'] ||= 'docker'
 
-require 'beaker-puppet'
-require 'beaker-rspec'
-require 'beaker/puppet_install_helper'
-require 'beaker/module_install_helper'
+configure_beaker do |host|
+  if fact_on(host, 'os.family') == 'RedHat'
+    unless fact_on(host, 'os.name') == 'Fedora'
+      # don't delete downloaded rpm for use with BEAKER_provision=no +
+      # BEAKER_destroy=no
+      on host, 'sed -i "s/keepcache=.*/keepcache=1/" /etc/yum.conf'
+    end
+    # refresh check if cache needs refresh on next yum command
+    on host, 'yum clean expire-cache'
 
-run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
-install_module_on(hosts)
-install_module_dependencies_on(hosts)
-
-RSpec.configure do |c|
-  # Readable test descriptions
-  c.formatter = :documentation
-
-  # Configure all nodes in nodeset
-  c.before :suite do
-    # Install module and dependencies
-    hosts.each do |host|
-      if fact_on(host, 'os.family') == 'RedHat'
-        # don't delete downloaded rpm for use with BEAKER_provision=no +
-        # BEAKER_destroy=no
-        on host, 'sed -i "s/keepcache=.*/keepcache=1/" /etc/yum.conf'
-        # refresh check if cache needs refresh on next yum command
-        on host, 'yum clean expire-cache'
-
-        major = fact_on(host, 'os.release.major')
-
-        if major == '7' && fact_on(host, 'os.name') == 'CentOS'
-          host.install_package('centos-release-scl-rh')
-          host.install_package('rh-redis5-redis')
-        end
-
-        baseurl = if ENV['PULPCORE_REPO_RELEASE']
-                    "https://fedorapeople.org/groups/katello/releases/yum/nightly/pulpcore/el#{major}/x86_64/"
-                  else
-                    "http://koji.katello.org/releases/yum/katello-nightly/pulpcore/el#{major}/x86_64/"
-                  end
-
-        on host, puppet_resource('yumrepo', 'pulpcore', "baseurl=#{baseurl}", 'gpgcheck=0')
-
-        if fact_on(host, 'os.selinux.enabled')
-          on host, puppet('resource', 'package', 'pulpcore-selinux', 'ensure=installed')
-        end
-      end
+    local_setup = File.join(__dir__, 'setup_acceptance_node.pp')
+    if File.exist?(local_setup)
+      apply_manifest_on(host, File.read(local_setup), catch_failures: true)
     end
   end
 end
@@ -64,7 +31,7 @@ end
 
 shared_examples 'the example' do |name|
   let(:pp) do
-    path = File.join(File.dirname(File.dirname(__FILE__)), 'examples', name)
+    path = File.join(File.dirname(__dir__), 'examples', name)
     File.read(path)
   end
 
