1098c9eeed082cbc93cf39abae24cc91c6358d25
William Bradford Clark <wclark@redhat.com>
2020-05-06 11:33:58 -0400

    set module_hotfix=1 for pulpcore repo on el8 acceptance spec

--- spec/setup_acceptance_node.pp
+++ spec/setup_acceptance_node.pp
@@ -23,9 +23,16 @@ case $major {
 # $baseurl = "https://fedorapeople.org/groups/katello/releases/yum/nightly/pulpcore/el${major}/x86_64/"
 $baseurl = "http://koji.katello.org/releases/yum/katello-nightly/pulpcore/el${major}/x86_64/"
 
+file { 'pulpcore-repo-extra-config':
+  path    => '/etc/yum/pulpcore.conf',
+  content => 'module_hotfixes=1',
+  before  => Yumrepo['pulpcore'],
+}
+
 yumrepo { 'pulpcore':
   baseurl  => $baseurl,
   gpgcheck => 0,
+  include  => 'file:///etc/yum/pulpcore.conf',
 }
 
 # Needed as a workaround for idempotency
