392ae50edda1839e5bfb511e90489ccf4be98f50
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-09-30 18:43:54 +0200

    Support Pulpcore 3.7

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -3,7 +3,7 @@
 # @param version
 #   The Pulpcore version to use
 class pulpcore::repo (
-  Pattern['^\d+\.\d+$'] $version = '3.6',
+  Pattern['^\d+\.\d+$'] $version = '3.7',
 ) {
   $context = {
     'version'   => $version,
