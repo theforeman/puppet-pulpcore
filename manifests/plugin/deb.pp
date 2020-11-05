c071bbb474e10e68882d7ccca3a75df46a682dbb
Justin Sherrill <jsherril@redhat.com>
2020-11-05 08:57:54 -0500

    support /pulp/deb proxying

--- manifests/plugin/deb.pp
+++ manifests/plugin/deb.pp
@@ -1,5 +1,33 @@
 # @summary Pulp Deb plugin
-class pulpcore::plugin::deb {
+# @param use_pulp2_content_route Whether to redirect the legacy (Pulp 2) URL, /pulp/deb/, to the content server
+class pulpcore::plugin::deb (
+  Boolean $use_pulp2_content_route = false,
+) {
+  if $use_pulp2_content_route {
+    $context = {
+      'directories' => [
+        {
+          'provider'        => 'location',
+          'path'            => '/pulp/deb',
+          'proxy_pass'      => [
+            {
+              'url' => $pulpcore::apache::content_url,
+            },
+          ],
+          'request_headers' => [
+            'unset X-CLIENT-CERT',
+            'set X-CLIENT-CERT "%{SSL_CLIENT_CERT}s" env=SSL_CLIENT_CERT',
+          ],
+        },
+      ],
+    }
+    $content = epp('pulpcore/apache-fragment.epp', $context)
+  } else {
+    $content = undef
+  }
+
   pulpcore::plugin { 'deb':
+    http_content  => $content,
+    https_content => $content,
   }
 }
