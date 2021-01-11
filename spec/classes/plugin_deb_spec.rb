463629bd3e526a549186d65db3f50016351fe268
Eric D. Helms <ericdhelms@gmail.com>
2021-01-11 10:52:27 -0500

    Add proxy params to plugin Pulp 2 content routes

--- spec/classes/plugin_deb_spec.rb
+++ spec/classes/plugin_deb_spec.rb
@@ -28,7 +28,29 @@ describe 'pulpcore::plugin::deb' do
             is_expected.to compile.with_all_deps
             is_expected.to contain_pulpcore__apache__fragment('plugin-deb')
             is_expected.to contain_apache__vhost__fragment('pulpcore-http-plugin-deb')
+              .with_content(
+<<CONTENT
+
+  <Location "/pulp/deb">
+    RequestHeader unset X-CLIENT-CERT
+    RequestHeader set X-CLIENT-CERT "%{SSL_CLIENT_CERT}s" env=SSL_CLIENT_CERT
+    ProxyPass unix:///run/pulpcore-content.sock|http://pulpcore-content/pulp/content timeout=600
+    ProxyPassReverse unix:///run/pulpcore-content.sock|http://pulpcore-content/pulp/content
+  </Location>
+CONTENT
+              )
             is_expected.to contain_apache__vhost__fragment('pulpcore-https-plugin-deb')
+              .with_content(
+<<CONTENT
+
+  <Location "/pulp/deb">
+    RequestHeader unset X-CLIENT-CERT
+    RequestHeader set X-CLIENT-CERT "%{SSL_CLIENT_CERT}s" env=SSL_CLIENT_CERT
+    ProxyPass unix:///run/pulpcore-content.sock|http://pulpcore-content/pulp/content timeout=600
+    ProxyPassReverse unix:///run/pulpcore-content.sock|http://pulpcore-content/pulp/content
+  </Location>
+CONTENT
+              )
           end
         end
       end
--- spec/classes/plugin_file_spec.rb
+++ spec/classes/plugin_file_spec.rb
@@ -18,6 +18,39 @@ describe 'pulpcore::plugin::file' do
             .that_subscribes_to('Class[Pulpcore::Install]')
             .that_notifies(['Class[Pulpcore::Database]', 'Class[Pulpcore::Service]'])
         end
+
+        context 'with pulp2 content route' do
+          let(:params) { { use_pulp2_content_route: true } }
+
+          it 'contains the Apache fragment' do
+            is_expected.to compile.with_all_deps
+            is_expected.to contain_pulpcore__apache__fragment('plugin-file')
+            is_expected.to contain_apache__vhost__fragment('pulpcore-http-plugin-file')
+              .with_content(
+<<CONTENT
+
+  <Location "/pulp/isos">
+    RequestHeader unset X-CLIENT-CERT
+    RequestHeader set X-CLIENT-CERT "%{SSL_CLIENT_CERT}s" env=SSL_CLIENT_CERT
+    ProxyPass unix:///run/pulpcore-content.sock|http://pulpcore-content/pulp/content timeout=600
+    ProxyPassReverse unix:///run/pulpcore-content.sock|http://pulpcore-content/pulp/content
+  </Location>
+CONTENT
+              )
+            is_expected.to contain_apache__vhost__fragment('pulpcore-https-plugin-file')
+              .with_content(
+<<CONTENT
+
+  <Location "/pulp/isos">
+    RequestHeader unset X-CLIENT-CERT
+    RequestHeader set X-CLIENT-CERT "%{SSL_CLIENT_CERT}s" env=SSL_CLIENT_CERT
+    ProxyPass unix:///run/pulpcore-content.sock|http://pulpcore-content/pulp/content timeout=600
+    ProxyPassReverse unix:///run/pulpcore-content.sock|http://pulpcore-content/pulp/content
+  </Location>
+CONTENT
+              )
+          end
+        end
       end
     end
   end
