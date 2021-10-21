85af5ccfba1db12b2964245d75580a17036e80c7
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2021-10-21 19:45:21 +0200

    Do not install subman-rhsm for certguard
    Since python3-certguard 1.4.0-3 this is solved in packaging. That clears
    the path for Pulp 3.15.

--- manifests/plugin/certguard.pp
+++ manifests/plugin/certguard.pp
@@ -1,8 +1,5 @@
 # @summary Pulp Certguard plugin
 class pulpcore::plugin::certguard {
-  package { 'python3-subscription-manager-rhsm':
-    ensure => present,
-  }
-  -> pulpcore::plugin { 'certguard':
+  pulpcore::plugin { 'certguard':
   }
 }
--- spec/classes/plugin_certguard_spec.rb
+++ spec/classes/plugin_certguard_spec.rb
@@ -6,7 +6,6 @@ describe 'pulpcore::plugin::certguard' do
       let(:facts) { os_facts }
 
       it { is_expected.to compile.with_all_deps }
-      it { is_expected.to contain_package('python3-subscription-manager-rhsm').with_ensure('present') }
       it { is_expected.to contain_package('pulpcore-plugin(certguard)') }
       it { is_expected.to contain_pulpcore__plugin('certguard') }
 
@@ -16,7 +15,6 @@ describe 'pulpcore::plugin::certguard' do
         it do
           is_expected.to compile.with_all_deps
           is_expected.to contain_pulpcore__plugin('certguard')
-            .that_requires('Package[python3-subscription-manager-rhsm]')
             .that_subscribes_to('Class[Pulpcore::Install]')
             .that_notifies(['Class[Pulpcore::Database]', 'Class[Pulpcore::Service]'])
         end
