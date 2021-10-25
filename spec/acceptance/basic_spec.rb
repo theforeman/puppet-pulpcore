1b446195f7a3435670b27e81185746fb6229b7c4
Evgeni Golov <evgeni@golov.de>
2021-10-25 13:26:56 +0200

    use libexec rq wrapper to call rq
    this ensures the right SELinux context is set
    and also allows to use the SCL (if there is any)

--- spec/acceptance/basic_spec.rb
+++ spec/acceptance/basic_spec.rb
@@ -79,7 +79,7 @@ describe 'basic installation' do
     its(:exit_status) { is_expected.to eq 0 }
   end
 
-  describe command("DJANGO_SETTINGS_MODULE=pulpcore.app.settings PULP_SETTINGS=/etc/pulp/settings.py rq info -c pulpcore.rqconfig") do
+  describe command("DJANGO_SETTINGS_MODULE=pulpcore.app.settings PULP_SETTINGS=/etc/pulp/settings.py /usr/libexec/pulpcore/rq info -c pulpcore.rqconfig") do
     its(:stdout) { is_expected.to match(/^0 workers, /) }
     its(:stdout) { is_expected.not_to match(/^resource-manager /) }
     its(:exit_status) { is_expected.to eq 0 }
