644250d46cf673e3624938fedf395e595f3d22d8
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2022-03-16 14:26:25 +0100

    Introduce a shared example to reduce duplication

---
+++ spec/support/acceptance/examples.rb
@@ -0,0 +1,69 @@
+shared_examples 'the default pulpcore application' do
+  certdir = '/etc/pulpcore-certs'
+
+  describe user('pulp') do
+    it { is_expected.to exist }
+    its(:uid) { is_expected.to be < 1000 }
+  end
+
+  describe group('pulp') do
+    it { is_expected.to exist }
+    its(:gid) { is_expected.to be < 1000 }
+  end
+
+  describe service('httpd') do
+    it { is_expected.to be_enabled }
+    it { is_expected.to be_running }
+  end
+
+  describe service('pulpcore-api') do
+    it { is_expected.to be_enabled }
+    it { is_expected.to be_running }
+  end
+
+  describe service('pulpcore-content') do
+    it { is_expected.to be_enabled }
+    it { is_expected.to be_running }
+  end
+
+  describe service('pulpcore-resource-manager') do
+    it { is_expected.not_to be_enabled }
+    it { is_expected.not_to be_running }
+  end
+
+  describe service('pulpcore-worker@1') do
+    it { is_expected.to be_enabled }
+    it { is_expected.to be_running }
+  end
+
+  describe port(80) do
+    it { is_expected.to be_listening }
+  end
+
+  describe port(443) do
+    it { is_expected.to be_listening }
+  end
+
+  describe curl_command("https://#{host_inventory['fqdn']}/pulp/api/v3/status/", cacert: "#{certdir}/ca-cert.pem") do
+    its(:response_code) { is_expected.to eq(200) }
+    its(:exit_status) { is_expected.to eq 0 }
+  end
+
+  describe command("PULP_SETTINGS=/etc/pulp/settings.py pulpcore-manager dumpdata auth.User") do
+    its(:stdout) { is_expected.to match(/auth\.user/) }
+    its(:exit_status) { is_expected.to eq 0 }
+  end
+
+  describe curl_command("https://#{host_inventory['fqdn']}/pulp/api/v3/", cacert: "#{certdir}/ca-cert.pem") do
+    # Requires authentication: https://github.com/pulp/pulpcore/issues/2340
+    its(:response_code) { is_expected.to eq(403) }
+    its(:exit_status) { is_expected.to eq 0 }
+  end
+
+  describe curl_command("https://#{host_inventory['fqdn']}/pulp/api/v3/",
+                        cacert: "#{certdir}/ca-cert.pem", key: "#{certdir}/client-key.pem", cert: "#{certdir}/client-cert.pem") do
+    its(:response_code) { is_expected.to eq(200) }
+    its(:body) { is_expected.to contain('artifacts') }
+    its(:exit_status) { is_expected.to eq 0 }
+  end
+end
