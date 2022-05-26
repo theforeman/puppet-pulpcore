c1331be3387362d3dd8e1d3a28f103d9b38a5fc7
ianballou <ianballou67@gmail.com>
2022-05-26 14:38:52 -0400

    Add Pulpcore 3.18 support

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -3,7 +3,7 @@
 # @param version
 #   The Pulpcore version to use
 class pulpcore::repo (
-  Pattern['^\d+\.\d+$'] $version = '3.17',
+  Pattern['^\d+\.\d+$'] $version = '3.18',
 ) {
   $dist_tag = "el${facts['os']['release']['major']}"
   $context = {
