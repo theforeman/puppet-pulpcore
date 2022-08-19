theforeman/puppet-pulpcore

commit 17a84f377da89c637468685b93bb61c3dc7bd369

Fixes #32766 - adds scheme and pulp content path to setting value

manifests/plugin/ansible.pp
@@ -15,8 +15,16 @@ class pulpcore::plugin::ansible(
     ],
   }
 
+  if ! $pulpcore::apache_https_vhost {
+    fail('HTTPS must be turned on for Ansible content')
+  } elsif $pulpcore::apache::https_port != 443 {
+    $external_content_url = "https://${pulpcore::servername}:${pulpcore::apache::https_port}${pulpcore::apache::content_path}"
+  } else {
+    $external_content_url = "https://${pulpcore::servername}${pulpcore::apache::content_path}"
+  }
+
   pulpcore::plugin { 'ansible':
-    config        => "ANSIBLE_API_HOSTNAME = \"${pulpcore::servername}\"\nANSIBLE_CONTENT_HOSTNAME = \"${pulpcore::servername}\"",
+    config        => "ANSIBLE_API_HOSTNAME = \"${pulpcore::servername}\"\nANSIBLE_CONTENT_HOSTNAME = \"${external_content_url}\"",
     https_content => epp('pulpcore/apache-fragment.epp', $context),
   }
 }

spec/classes/plugin_ansible_spec.rb
@@ -25,7 +25,7 @@ CONTENT
                            .with_content(expected_vhost_content)
         is_expected.to contain_concat__fragment('plugin-ansible')
           .with_content(/^ANSIBLE_API_HOSTNAME = "foo.example.com"/)
-          .with_content(/^ANSIBLE_CONTENT_HOSTNAME = "foo.example.com"/)
+          .with_content(%r{^ANSIBLE_CONTENT_HOSTNAME = "https://foo.example.com/pulp/content"})
       end
     end
   end

commit cd41f4c0452ed77479f932554d568ba61e8381c0

Fixes #35390 - set ANSIBLE_API_HOSTNAME *with* scheme
Fixes: 17a84f377da89c637468685b93bb61c3dc7bd369
Fixes: 6ed8be4ec1dd8170fdf8e373019164ac15cb2dec

See https://github.com/pulp/pulp_ansible/blob/0.13/docs/settings.rst#ansible_api_hostname

manifests/plugin/ansible.pp
@@ -19,12 +19,14 @@ class pulpcore::plugin::ansible (
     fail('HTTPS must be turned on for Ansible content')
   } elsif $pulpcore::apache::https_port != 443 {
     $external_content_url = "https://${pulpcore::servername}:${pulpcore::apache::https_port}${pulpcore::apache::content_path}"
+    $external_api_url = "https://${pulpcore::servername}:${pulpcore::apache::https_port}"
   } else {
     $external_content_url = "https://${pulpcore::servername}${pulpcore::apache::content_path}"
+    $external_api_url = "https://${pulpcore::servername}"
   }
 
   pulpcore::plugin { 'ansible':
-    config        => "ANSIBLE_API_HOSTNAME = \"${pulpcore::servername}\"\nANSIBLE_CONTENT_HOSTNAME = \"${external_content_url}\"",
+    config        => "ANSIBLE_API_HOSTNAME = \"${external_api_url}\"\nANSIBLE_CONTENT_HOSTNAME = \"${external_content_url}\"",
     https_content => epp('pulpcore/apache-fragment.epp', $context),
   }
 }

spec/classes/plugin_ansible_spec.rb
@@ -24,7 +24,7 @@ CONTENT
         is_expected.to contain_apache__vhost__fragment('pulpcore-https-plugin-ansible')
                            .with_content(expected_vhost_content)
         is_expected.to contain_concat__fragment('plugin-ansible')
-          .with_content(/^ANSIBLE_API_HOSTNAME = "foo.example.com"/)
+          .with_content(%r{^ANSIBLE_API_HOSTNAME = "https://foo.example.com"})
           .with_content(%r{^ANSIBLE_CONTENT_HOSTNAME = "https://foo.example.com/pulp/content"})
       end
     end