cf32e9b3d74691ce74b32e48b90b399704396ed0
ianballou <ianballou67@gmail.com>
2023-11-08 10:14:49 -0500

    Fixes #36902 - Support Pulpcore 3.39, drop older versions

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -8,7 +8,7 @@
 #   An optional value for gpgkey to be used for yumrepo, instead of the default.
 #   If an empty string is passed, gpgcheck will be disabled.
 class pulpcore::repo (
-  Variant[Enum['nightly'], Pattern['^\d+\.\d+$']] $version = '3.28',
+  Variant[Enum['nightly'], Pattern['^\d+\.\d+$']] $version = '3.39',
   Optional[Stdlib::HTTPUrl] $baseurl = undef,
   Optional[String[0]] $gpgkey = undef,
 ) {
@@ -18,21 +18,9 @@ class pulpcore::repo (
     descr    => "Pulpcore ${version}",
     baseurl  => pick($baseurl, "https://yum.theforeman.org/pulpcore/${version}/${dist_tag}/\$basearch"),
     enabled  => '1',
-    gpgcheck => if $gpgkey == '' { '0' } else { '1' },
+    gpgcheck => if $gpgkey == '' or $version == 'nightly' { '0' } else { '1' },
     gpgkey   => pick($gpgkey, "https://yum.theforeman.org/pulpcore/${version}/GPG-RPM-KEY-pulpcore"),
     notify   => Anchor['pulpcore::repo'],
-  }
-
-  # Only EL8 has DNF modules
-  if $dist_tag == 'el8' {
-    package { 'pulpcore-dnf-module':
-      ensure      => $dist_tag,
-      name        => 'pulpcore',
-      enable_only => true,
-      provider    => 'dnfmodule',
-      require     => Yumrepo['pulpcore'],
-      notify      => Anchor['pulpcore::repo'],
-    }
   }
 
   # An anchor is used because it can be collected
