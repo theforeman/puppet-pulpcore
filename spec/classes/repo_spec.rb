40fad03fe21346d0df5290207b94922016efdb5c
Eric D. Helms <ericdhelms@gmail.com>
2023-11-15 13:04:09 -0500

    Support nightly repository version

--- spec/classes/repo_spec.rb
+++ spec/classes/repo_spec.rb
@@ -5,26 +5,43 @@ describe 'pulpcore::repo' do
     context "on #{os}" do
       let(:facts) { os_facts }
 
-      it { is_expected.to compile.with_all_deps }
-      it do
-        is_expected.to contain_anchor('pulpcore::repo')
-        is_expected.to contain_file('/etc/yum.repos.d/pulpcore.repo')
-          .with_content(%r{^baseurl=https://yum.theforeman.org/pulpcore/\d+\.\d+/el\d+/\$basearch$})
-          .that_notifies('Anchor[pulpcore::repo]')
-      end
-
-      if os_facts[:os]['release']['major'] == '8'
-        it 'configures the pulpcore module' do
-          is_expected.to contain_package('pulpcore-dnf-module')
-            .with_name('pulpcore')
-            .with_ensure(/^el\d+/)
-            .with_enable_only(true)
-            .with_provider('dnfmodule')
-            .that_requires('File[/etc/yum.repos.d/pulpcore.repo]')
+      describe "with default values" do
+        it { is_expected.to compile.with_all_deps }
+        it do
+          is_expected.to contain_anchor('pulpcore::repo')
+          is_expected.to contain_file('/etc/yum.repos.d/pulpcore.repo')
+            .with_content(%r{^baseurl=https://yum.theforeman.org/pulpcore/\d+\.\d+/el\d+/\$basearch$})
             .that_notifies('Anchor[pulpcore::repo]')
         end
-      else
-        it { is_expected.not_to contain_package('pulpcore-dnf-module') }
+
+        if os_facts[:os]['release']['major'] == '8'
+          it 'configures the pulpcore module' do
+            is_expected.to contain_package('pulpcore-dnf-module')
+              .with_name('pulpcore')
+              .with_ensure(/^el\d+/)
+              .with_enable_only(true)
+              .with_provider('dnfmodule')
+              .that_requires('File[/etc/yum.repos.d/pulpcore.repo]')
+              .that_notifies('Anchor[pulpcore::repo]')
+          end
+        else
+          it { is_expected.not_to contain_package('pulpcore-dnf-module') }
+        end
+      end
+
+      describe "with nightly version" do
+        let :params do
+          {
+            version: 'nightly'
+          }
+        end
+
+        it { is_expected.to compile.with_all_deps }
+        it do
+          is_expected.to contain_file('/etc/yum.repos.d/pulpcore.repo')
+            .with_content(%r{^baseurl=https://yum.theforeman.org/pulpcore/nightly/el\d+/\$basearch$})
+            .that_notifies('Anchor[pulpcore::repo]')
+        end
       end
     end
   end
