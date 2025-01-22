76316b37fc8343f7ec0906cafdcbb24575080702
Evgeni Golov <evgeni@golov.de>
2025-01-22 08:23:57 +0100

    Add support for Pulpcore 3.63

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -8,7 +8,7 @@
 #   An optional value for gpgkey to be used for yumrepo, instead of the default.
 #   If an empty string is passed, gpgcheck will be disabled.
 class pulpcore::repo (
-  Variant[Enum['nightly'], Pattern['^\d+\.\d+$']] $version = '3.39',
+  Variant[Enum['nightly'], Pattern['^\d+\.\d+$']] $version = '3.63',
   Optional[Stdlib::HTTPUrl] $baseurl = undef,
   Optional[String[0]] $gpgkey = undef,
 ) {
