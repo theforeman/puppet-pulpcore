theforeman/puppet-pulpcore

commit d4692522bb18cc2ec1adddd1eaba34db3b50ad61

Refs #28901 - add setting postgresql_db_user

manifests/database.pp
@@ -5,7 +5,7 @@ class pulpcore::database {
     include postgresql::client
     include postgresql::server
     postgresql::server::db { $pulpcore::postgresql_db_name:
-      user     => $pulpcore::user,
+      user     => $pulpcore::postgresql_db_user,
       password => postgresql_password($pulpcore::user, $pulpcore::postgresql_db_password),
       before   => Pulpcore::Admin['migrate --noinput'],
     }

manifests/init.pp
@@ -36,6 +36,9 @@
 # @param postgresql_db_name
 #   Name of Pulp database
 #
+# @param postgresql_db_user
+#   Pulp database user
+#
 # @param postgresql_db_password
 #   Password of Pulp database
 #
@@ -75,6 +78,7 @@ class pulpcore (
   Stdlib::Port $content_port = 24816,
   Stdlib::Absolutepath $webserver_static_dir = '/var/lib/pulp/docroot',
   String $postgresql_db_name = 'pulpcore',
+  String $postgresql_db_user = 'pulp',
   String $postgresql_db_password = extlib::cache_data('pulpcore_cache_data', 'db_password', extlib::random_password(32)),
   Stdlib::Host $postgresql_db_host = 'localhost',
   Stdlib::Port $postgresql_db_port = 5432,

spec/acceptance/basic_spec.rb
@@ -16,7 +16,9 @@ describe 'basic installation' do
     class { 'redis::globals':
       scl => 'rh-redis5',
     }
-    include pulpcore
+    class { 'pulpcore':
+      postgresql_db_user => 'pulp',
+    }
     PUPPET
   }
 

templates/settings.py.erb
@@ -5,7 +5,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': '<%= scope['pulpcore::postgresql_db_name'] %>',
-        'USER': '<%= scope['pulpcore::user'] %>',
+        'USER': '<%= scope['pulpcore::postgresql_db_user'] %>',
         'PASSWORD': '<%= scope['pulpcore::postgresql_db_password'] %>',
         'HOST': '<%= scope['pulpcore::postgresql_db_host'] %>',
         'PORT': '<%= scope['pulpcore::postgresql_db_port'] %>',

commit 9c7b7c74c39f9a39846d76f63e09e7716af186f3

use the right username when computing the db password hash
some people use other names than `pulp` for their DB user

Fixes: d4692522bb18cc2ec1adddd1eaba34db3b50ad61

manifests/database.pp
@@ -11,7 +11,7 @@ class pulpcore::database (
     ensure_packages(['glibc-langpack-en'])
     postgresql::server::db { $pulpcore::postgresql_db_name:
       user     => $pulpcore::postgresql_db_user,
-      password => postgresql::postgresql_password($pulpcore::user, $pulpcore::postgresql_db_password),
+      password => postgresql::postgresql_password($pulpcore::postgresql_db_user, $pulpcore::postgresql_db_password),
       encoding => 'utf8',
       locale   => 'en_US.utf8',
       before   => Pulpcore::Admin['migrate --noinput'],